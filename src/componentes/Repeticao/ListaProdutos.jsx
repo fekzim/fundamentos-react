64ec6526f01a56a4a72d1047209ba37f2288ab3c
Felipe Roberto <56833987+fekzim@users.noreply.github.com>
2022-03-16 16:39:24 -0300

    Update ListaProdutos.jsx

--- src/componentes/Repeticao/ListaProdutos.jsx
+++ src/componentes/Repeticao/ListaProdutos.jsx
@@ -7,7 +7,7 @@ export default function ListaProdutos (props) {
     function LinhasTabela(){
         return produtos.map((produto, i) => {
             return(
-                <tr key={produto.id} className={i % 2 == 0 ? 'Par' : 'Impar'}>
+                <tr key={produto.id} className={i % 2 === 0 ? 'Par' : 'Impar'}>
                 <td>{produto.id}</td>
                 <td>{produto.nome}</td>
                 <td>{produto.preco}</td>
