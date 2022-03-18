f09ecabdf30b4fc61985cdd1a1791a34535aa097
Felipe Roberto <56833987+fekzim@users.noreply.github.com>
2022-03-18 00:50:19 -0300

    Removido comentarios
    // Para passar propriedades para um componente você deve ir até ele e escrever a "variavel" e o valor, dentro do componente você ira escrever o nome
    // de como sera recebido o valor como props, e la dentro ira escrever por exemplo "props.variavel", assim você envia props
    
    // Propriedades são apenas leitura, se você quer fazer um tratamento de informação tera que receber o valor em uma variavel dentro do componente que estava
    //recebendo o props e la sim fazer o tratamento, states seriam propriedades que podem ser alteradas
    
    //Em si todos os componentes devem estar envolvidos por alguma tag como uma div ou um fragment "<> </>"

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './App'
-// Para passar propriedades para um componente você deve ir até ele e escrever a "variavel" e o valor, dentro do componente você ira escrever o nome
-// de como sera recebido o valor como props, e la dentro ira escrever por exemplo "props.variavel", assim você envia props
-
-// Propriedades são apenas leitura, se você quer fazer um tratamento de informação tera que receber o valor em uma variavel dentro do componente que estava
-//recebendo o props e la sim fazer o tratamento, states seriam propriedades que podem ser alteradas
-
-//Em si todos os componentes devem estar envolvidos por alguma tag como uma div ou um fragment "<> </>"
 ReactDOM.render(
     <App/>, 
 document.getElementById('root'))
